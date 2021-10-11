687fd540281e0f4a9fbe1a81ed63b13748f03a42
Koomefranklin <87543686+Koomefranklin@users.noreply.github.com>
2021-10-12 01:40:04 +0300

    Update regional calendar.py

--- regional calendar.py
+++ regional calendar.py
@@ -1,7 +1,8 @@
+# importing date and time to work with dates
 from datetime import datetime
-from dateutil.parser import parse
-b = input()
-a = parse(b, datefirst)
+from dateutil.parser import parse #parse helps read strings as dates
+b = input("Imput the time in mm/dd/yyyy formart")#getting user input dates
+a = parse(b, datefirst) #turning the string to date fprmart
 c = datetime.date(a)
 d = c.strftime("%#d/%#m/%Y")
 print(d)
