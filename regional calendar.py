7f68827c3de16eb83d24b791629da6ca4c609a27
Frank <87543686+Koomefranklin@users.noreply.github.com>
2023-02-25 12:19:54 +0300

    Delete regional calendar.py

--- regional calendar.py
+++
@@ -1,8 +0,0 @@
-# importing date and time to work with dates
-from datetime import datetime
-from dateutil.parser import parse #parse helps read strings as dates
-b = input("Imput the time in mm/dd/yyyy formart")#getting user input dates
-a = parse(b, datefirst) #turning the string to date fprmart
-c = datetime.date(a)
-d = c.strftime("%#d/%#m/%Y")
-print(d)
